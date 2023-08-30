25c33d84f9b122952769ac44b21155faa54dc0bc
lc <1969956387@qq.com>
2023-08-30 21:54:37 +0800

    feat: v1.0.0 封版

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'system',
+  base: 'systemAdmin',
   plugins: [react()],
   resolve: {
     extensions: [".js", ".ts", ".tsx", ".jsx"],
